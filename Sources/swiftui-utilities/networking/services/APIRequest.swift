915702392600dc0b957a620e490320d22ab45de7
Nikhil Jaggi <nkhljgg@gmail.com>
2023-01-08 16:09:46 +0530

    Fixed access level issue

--- Sources/swiftui-utilities/networking/services/APIRequest.swift
+++ Sources/swiftui-utilities/networking/services/APIRequest.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 public struct APIRequest {
+    
+    public init(method: HTTPMethod, endpoint: String, body: Encodable?) {
+        self.method = method
+        self.endpoint = endpoint
+        self.body = body
+    }
+    
     let method: HTTPMethod
     let endpoint: String
     let body:Encodable?
