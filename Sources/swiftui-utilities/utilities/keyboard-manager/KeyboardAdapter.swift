d1c82860d737424c1abf482edd20dd9460ad2ed5
Nikhil Jaggi <nkhljgg@gmail.com>
2023-02-25 16:45:57 +0530

    Added keyboard adapter

---
+++ Sources/swiftui-utilities/utilities/keyboard-manager/KeyboardAdapter.swift
@@ -0,0 +1,46 @@
+//
+//  KeyboardAdapter.swift
+//  
+//
+//  Created by Nikhil on 25/02/23.
+//
+
+import SwiftUI
+import Combine
+
+public struct KeyboardAdapter: ViewModifier {
+    @State var currentHeight: CGFloat = 0
+    
+   public func body(content: Content) -> some View {
+        GeometryReader { geometry in
+            content
+                .padding(.bottom, self.currentHeight)
+                .onAppear(perform: {
+                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillShowNotification)
+                        .merge(with: NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillChangeFrameNotification))
+                        .compactMap { notification in
+                            withAnimation(.easeOut(duration: 0.16)) {
+                                notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+                            }
+                    }
+                    .map { rect in
+                        rect.height - geometry.safeAreaInsets.bottom
+                    }
+                    .subscribe(Subscribers.Assign(object: self, keyPath: \.currentHeight))
+                    
+                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillHideNotification)
+                        .compactMap { notification in
+                            CGFloat.zero
+                    }
+                    .subscribe(Subscribers.Assign(object: self, keyPath: \.currentHeight))
+                })
+        }
+    }
+}
+
+public extension View {
+    func adaptToKeyboard() -> some View {
+        return modifier(KeyboardAdapter())
+    }
+}
+
